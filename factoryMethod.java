e489ce89bce2ecd845ac89bbd7bf4ff5c427c5f9
Macapobrerommel <macapobrerommel@gmail.com>
2026-01-31 14:39:00 +0800

    Update factoryMethod.java

--- factoryMethod.java
+++ factoryMethod.java
@@ -1,52 +1,90 @@
-// Product Interface
+package your.package.name;
+
+/**
+ * Product interface.
+ */
 interface Product {
+
+    /**
+     * Displays product information.
+     */
     void display();
 }
 
-// Concrete Products
+/**
+ * Concrete Product A.
+ */
 class ConcreteProductA implements Product {
+
     @Override
     public void display() {
         System.out.println("This is Concrete Product A.");
     }
 }
 
+/**
+ * Concrete Product B.
+ */
 class ConcreteProductB implements Product {
+
     @Override
     public void display() {
         System.out.println("This is Concrete Product B.");
     }
 }
 
-// Factory Interface
+/**
+ * Factory interface.
+ */
 interface Factory {
+
+    /**
+     * Creates a product.
+     *
+     * @return a new Product instance
+     */
     Product factoryMethod();
 }
 
-// Concrete Factories
+/**
+ * Concrete Factory A.
+ */
 class ConcreteFactoryA implements Factory {
+
     @Override
     public Product factoryMethod() {
         return new ConcreteProductA();
     }
 }
 
+/**
+ * Concrete Factory B.
+ */
 class ConcreteFactoryB implements Factory {
+
     @Override
     public Product factoryMethod() {
         return new ConcreteProductB();
     }
 }
 
-// Client Code
+/**
+ * Client code demonstrating the Factory Method pattern.
+ */
 public class FactoryMethodExample {
-    public static void main(String[] args) {
-        Factory factoryA = new ConcreteFactoryA();
-        Product productA = factoryA.factoryMethod();
+
+    /**
+     * Application entry point.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(final String[] args) {
+        final Factory factoryA = new ConcreteFactoryA();
+        final Product productA = factoryA.factoryMethod();
         productA.display();
 
-        Factory factoryB = new ConcreteFactoryB();
-        Product productB = factoryB.factoryMethod();
+        final Factory factoryB = new ConcreteFactoryB();
+        final Product productB = factoryB.factoryMethod();
         productB.display();
     }
 }
